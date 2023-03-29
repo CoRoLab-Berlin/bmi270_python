74ba343f0bdb06110c56719aa3704d7ce7bb63af
Kevin <s50948@bht-berlin.de>
2023-03-29 15:58:37 +0200

    Fallback from threading to sleep (for now)
    - threading causes problems with 200hz ODR

--- examples/base.py
+++ examples/base.py
@@ -5,6 +5,9 @@ import time
 from bmi270.BMI270 import *
 from bmi270.UDP import *
 
+# from src.bmi270.BMI270 import *
+# from src.bmi270.UDP import *
+
 # -------------------------------------------------
 # INITIALIZATION
 # -------------------------------------------------
@@ -25,6 +28,8 @@ BMI270_1.set_acc_odr(ACC_ODR_200)
 BMI270_1.set_gyr_odr(GYR_ODR_200)
 BMI270_1.set_acc_bwp(ACC_BWP_OSR4)
 BMI270_1.set_gyr_bwp(GYR_BWP_OSR4)
+BMI270_1.enable_fifo_streaming()
+BMI270_1.disable_fifo_header()
 
 BMI270_2.set_mode(PERFORMANCE_MODE)
 BMI270_2.set_acc_range(ACC_RANGE_2G)
@@ -33,17 +38,19 @@ BMI270_2.set_acc_odr(ACC_ODR_200)
 BMI270_2.set_gyr_odr(GYR_ODR_200)
 BMI270_2.set_acc_bwp(ACC_BWP_OSR4)
 BMI270_2.set_gyr_bwp(GYR_BWP_OSR4)
+BMI270_2.enable_fifo_streaming()
+BMI270_2.disable_fifo_header()
 
 # -------------------------------------------------
 # NETWORK CONFIGURATION
 # -------------------------------------------------
 
 RECEIVER_IP = ''
-RECEIVER_PORT = 33771
+RECEIVER_PORT = 12345
 SENDER_IP = ''
-SENDER_PORT = 33881
+SENDER_PORT = 12345
 
-Sender = UDP(RECEIVER_IP, RECEIVER_PORT, SENDER_IP, SENDER_PORT)
+Network = UDP(RECEIVER_IP, RECEIVER_PORT, SENDER_IP, SENDER_PORT)
 
 # -------------------------------------------------
 # FUNCTIONS
@@ -52,59 +59,40 @@ Sender = UDP(RECEIVER_IP, RECEIVER_PORT, SENDER_IP, SENDER_PORT)
 
 def print_seconds():
     threading.Timer(1.0, print_seconds).start()
-    print("-" * 130, " ", int(time.time() - start_time), "s")
+    print("-" * 80, " ", int(time.time() - start_time), "s")
 
 
 def UDP_send_data():
-    threading.Timer(0.02, UDP_send_data).start()
-    data_bytes = Sender.pack_data(BMI270_1.get_raw_acc_data(), BMI270_1.get_raw_gyr_data(), BMI270_2.get_raw_acc_data(), BMI270_2.get_raw_gyr_data())
-    Sender.send_data(data_bytes)
+    # threading.Timer(HERTZ_100, UDP_send_data).start()
+    data_bytes = Network.pack_data(BMI270_1.get_sensor_time(), BMI270_1.get_raw_acc_data(), BMI270_1.get_raw_gyr_data(),
+                                   BMI270_2.get_sensor_time(), BMI270_2.get_raw_acc_data(), BMI270_2.get_raw_gyr_data())
+    Network.send_data(data_bytes)
+    sleep(HERTZ_200)
 
 
 # -------------------------------------------------
 # MAIN
 # -------------------------------------------------
 
+print("\nStarting in 3 seconds...")
+time.sleep(3)
 start_time = time.time()
 
 def main():
     print_seconds()
-    UDP_send_data()
 
+    while True:
+        UDP_send_data()
 
 if __name__ == "__main__":
     main()
 
 
+
+
 # -------------------------------------------------
-# DATA ACQUISITION TESTING
+# H5 FILE
 # -------------------------------------------------
-
-# accel_array_1 = np.zeros((1, 3))
-# gyro_array_1 = np.zeros((1, 3))
-# accel_array_2 = np.zeros((1, 3))
-# gyro_array_2 = np.zeros((1, 3))
-
-# try:
-#     while True:
-#             # BMI270_1.send_gyr_data()
-
-#             # accel_data_1 = BMI270_1.get_acc_data()
-#             # gyro_data_1 = BMI270_1.get_gyr_data()
-#             # accel_data_2 = BMI270_2.get_acc_data()
-#             # gyro_data_2 = BMI270_2.get_gyr_data()
-
-#             # accel_array_1 = np.append(accel_array_1, accel_data_1, axis=0)
-#             # gyro_array_1 = np.append(gyro_array_1, gyro_data_1, axis=0)
-#             # accel_array_2 = np.append(accel_array_2, accel_data_2, axis=0)
-#             # gyro_array_2 = np.append(gyro_array_2, gyro_data_2, axis=0)
-
-#             # print(f'1-ACCEL: {accel_data_1}'.ljust(33), f'1-GYRO: {gyro_data_1}'.ljust(33), f'2-ACCEL: {accel_data_2}'.ljust(33), f'GYRO: {gyro_data_2}'.ljust(33))
-
-#             sleep(5)
-# except KeyboardInterrupt:
-#     print("\n---- DATA STOPPED ----")
-#     pass
 
 # # Save as H5 file
 # if False:
